d15e4be1d5699e1a990c7cd666d91984d45163cf
Arithmetics <brock.m.tillotson@gmail.com>
2020-08-30 13:30:37 -0700

    deleting all, this repo is incomplete, but has some nice code

--- src/graphql/Models/Scalar.ts
+++
@@ -1,8 +0,0 @@
-import { GraphQLDate } from 'graphql-iso-date';
-import { GraphQLUpload } from 'graphql-upload';
-import { asNexusMethod } from '@nexus/schema';
-
-export const Upload = GraphQLUpload;
-export const DateTime = GraphQLDate;
-export const GQLDate = asNexusMethod(GraphQLDate, 'date');
-// export const GenderEnum = asNexusMethod(Gender, 'gender');
