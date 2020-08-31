6e483051de6a59c3ddebcccc1dd96bc77d99b251
Arithmetics <brock.m.tillotson@gmail.com>
2020-08-30 21:32:38 -0700

    switching to graphql-shied

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,16 @@
-import { Context } from './context';
-import path from 'path';
-import { verify } from 'jsonwebtoken';
+import { Context } from "./context";
+import path from "path";
+import { verify } from "jsonwebtoken";
 
 const env = process.env.NODE_ENV;
-const envPath = env === 'development'
-  ? path.resolve(__dirname, '../dotenv/dev.env')
-  : env === 'test'
-    ? path.resolve(__dirname, '../dotenv/test.env')
-    : path.resolve(__dirname, '../dotenv/.env');
+const envPath =
+  env === "development"
+    ? path.resolve(__dirname, "../dotenv/dev.env")
+    : env === "test"
+    ? path.resolve(__dirname, "../dotenv/test.env")
+    : path.resolve(__dirname, "../dotenv/.env");
 
-require('dotenv').config({ path: envPath });
+require("dotenv").config({ path: envPath });
 
 const { JWT_SECRET } = process.env;
 export const APP_SECRET = JWT_SECRET;
@@ -19,9 +20,9 @@ interface Token {
 }
 
 export function getUserId(context: Context): string {
-  const Authorization = context.request.req.get('Authorization');
+  const Authorization = context.request.req.get("Authorization");
   if (Authorization) {
-    const token = Authorization.replace('Bearer ', '');
+    const token = Authorization.replace("Bearer ", "");
     const verifiedToken = verify(token, APP_SECRET) as Token;
     return verifiedToken && verifiedToken.userId;
   }
