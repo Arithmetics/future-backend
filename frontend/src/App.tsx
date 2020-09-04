c7bc54563c09515ba2ffc055d50cd49f1be99a8e
Arithmetics <brock.m.tillotson@gmail.com>
2020-09-03 20:09:04 -0700

    installed packages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,7 @@ function App(): JSX.Element {
   return (
     <BumbagProvider>
       <Box>
-        <PageContent>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu
-          bibendum at varius vel. Volutpat sed cras ornare arcu dui. Faucibus
-          scelerisque eleifend donec pretium vulputate sapien nec.
-        </PageContent>
+        <PageContent>Here be my page</PageContent>
       </Box>
     </BumbagProvider>
   );
